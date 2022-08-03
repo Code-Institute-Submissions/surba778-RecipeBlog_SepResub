389af1a7733058919a8c9301207e73fdb38fb4f4
surba778 <surba778@gmail.com>
2022-08-03 13:16:16 +0000

    Added testcases in the blog

---
+++ blog/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from blog.models import Category
+
+
+class TestBlogModels(TestCase):
+
+    def test_model_Category(self):
+        name = Category.objects.create(name="Django Testing")
+        self.assertNotEqual(name, '')
+
+    def test_category_string_method_returns_name(self):
+        name = Category.objects.create(name='test blog name')
+        self.assertEqual(str(name), 'test blog name')
+
+    def test_get_absolute_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)

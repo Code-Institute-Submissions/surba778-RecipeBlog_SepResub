07eb77556f1a70da25f9954600c5a02e55e65383
surba778 <surba778@gmail.com>
2022-09-14 13:18:46 +0000

    fix images place in media folder

--- blog/forms.py
+++ blog/forms.py
@@ -4,12 +4,6 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'form-control',
-        'placeholder': 'Type your comment',
-        'id': 'usercomment',
-        'rows': '4'
-    }))
     class Meta:
         model = Comment
         fields = ('body',)
